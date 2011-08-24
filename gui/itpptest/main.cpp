45addcd87fcb278fae0a5209e8dbb1cf5eea2ce7
Alvaro Prieto <axp8664@rit.edu>
2011-08-24 11:07:11 -0400

    Reading csv value into array works. Initial ICA code seems to work. Getting stuck on .separate() during the reconstruction process.

--- gui/itpptest/main.cpp
+++ gui/itpptest/main.cpp
@@ -6,18 +6,18 @@ using namespace itpp;
 
 using std::cout;
 using std::endl;
-
-#define TOTAL_LEADS (12)
+#define TOTAL_LEADS (15)
 #define INBUFSIZE (1024)
-#define MAX_SAMPLES (1000)
 
-void parse_line( char*, double* );
-uint8_t parse_table( FILE* , double p_value_table[][MAX_SAMPLES] );
+void parse_line( char*, mat* );
+uint8_t parse_table( FILE* , mat* );
 
 int main( int32_t argc, char *argv[] )
 {
   FILE *fp_in; 
-  double samples[TOTAL_LEADS][MAX_SAMPLES];
+  mat *samples;
+  
+  samples = new mat;
   
   // Make sure the filename is included
   if ( argc < 2 )
@@ -35,39 +35,89 @@ int main( int32_t argc, char *argv[] )
     return 1;
   }
   
+  // Read in CSV file and populate samples matrix
   parse_table(fp_in, samples);
   
+  // Close input file
   fclose(fp_in);
+  
+  //cout << *samples << endl;
+  
+  mat *leads_initial = new mat;
+  mat *leads_to_reconstruct = new mat;
+  
+  // New leads used to generate the ICs for each new beat
+  mat *leads_new = new mat;
+  
+  // Leads I(0),II(1), and V2(7)
+  leads_initial->append_row( samples->get_col(0) );
+  leads_initial->append_row( samples->get_col(1) );
+  leads_initial->append_row( samples->get_col(7) );
+  
+ 
+ 
+  // Whichever leads are not used to generate ICs
+  leads_to_reconstruct->append_row( samples->get_col(2) );
+  leads_to_reconstruct->append_row( samples->get_col(3) );
+  leads_to_reconstruct->append_row( samples->get_col(4) );
+  leads_to_reconstruct->append_row( samples->get_col(5) );
+  leads_to_reconstruct->append_row( samples->get_col(6) );
+  leads_to_reconstruct->append_row( samples->get_col(8) );
+  leads_to_reconstruct->append_row( samples->get_col(9) );
+  leads_to_reconstruct->append_row( samples->get_col(10) );
+  leads_to_reconstruct->append_row( samples->get_col(11) );
+  leads_to_reconstruct->append_row( samples->get_col(12) );
+  leads_to_reconstruct->append_row( samples->get_col(13) );
+  leads_to_reconstruct->append_row( samples->get_col(14) );
 
-#if 0  
-  mat leads_to_reconstruct; // Whichever leads are not used to generate ICs
-  mat leads_initial; // just the 3 leads (I(1),II(2), and V2(8))
-  mat leads_new; // New leads used to generate the ICs for each new beat
+
+  cout << "Starting fastica" << endl;
   
   // Training  
-  Fast_ICA fastica(leads_initial); 
+  Fast_ICA fastica(*leads_initial); 
   fastica.set_nrof_independent_components(3);
   fastica.set_approach(FICA_APPROACH_DEFL);
   fastica.separate();
-    
+  
+  cout << "done separating" << endl;
+  
   mat ICs_initial = fastica.get_independent_components();
+  
+  cout << "done getting ICs" << endl;
+  
   mat A_initial = fastica.get_mixing_matrix();
+  
+  cout << "done getting mixing matrix" << endl;
+         
 
   // Generate transform from IC to remaining leads
-  mat reconst_transform = leads_to_reconstruct * ICs_initial.T() * 
-                                  inv(ICs_initial * ICs_initial.T() );
+  mat reconst_transform = (*leads_to_reconstruct) * ICs_initial.T() *
+                                  inv( ICs_initial * ICs_initial.T() );
+                                  
 
   // Reconstruction
-  Fast_ICA fastica2(leads_new);
+  Fast_ICA fastica2(*leads_initial);
+  
   fastica2.set_nrof_independent_components(3);
+ 
   fastica2.set_approach(FICA_APPROACH_DEFL);
+
   fastica2.set_init_guess(A_initial);
-  fastica2.separate();
+
+  fastica2.separate();    
   
   mat ICs_current = fastica2.get_independent_components();
+  
   mat A_current = fastica2.get_mixing_matrix();
   mat leads_reconst = reconst_transform * ICs_current;
-#endif
+
+
+  // Cleanup
+  delete leads_to_reconstruct;
+  delete leads_initial;
+  delete leads_new; 
+  
+  delete samples;
 
   return 0;
 
@@ -79,25 +129,22 @@ int main( int32_t argc, char *argv[] )
  *
  * @brief Parse line from csv file an populate array row with contents
  * ****************************************************************************/
-void parse_line ( char* csv_line, double* output_line )
+void parse_line ( char* csv_line, mat* output_line )
 {
   char *p_item;
   uint16_t item_index = 0;
+  vec line(TOTAL_LEADS);
   
   p_item = strtok( csv_line, "," );
   while( NULL != p_item )
   {    
     // Convert string to RSSI value and store in output_line
-    //output_line[item_index] = strtod( p_item, NULL );
-    printf("%g", strtod( p_item, NULL ));
+    line[item_index] = strtod( p_item, NULL );
     item_index++;
-    p_item = strtok( NULL, "," );
-    
-    if( NULL != p_item )
-    {
-      printf(",");
-    }
+    p_item = strtok( NULL, "," );    
   }
+  output_line->append_row( line );
+  
 }
 
 /*******************************************************************************
@@ -106,27 +153,19 @@ void parse_line ( char* csv_line, double* output_line )
  *
  * @brief Read lines from CSV file and parse them until an empty line is found
  * ****************************************************************************/
-uint8_t parse_table ( FILE* fp_csv_file, double p_value_table[][MAX_SAMPLES] )
+uint8_t parse_table ( FILE* fp_csv_file, mat* p_value_table )
 {
   char csv_line[INBUFSIZE];   // Buffer for reading a line in the file
   uint16_t line_index = 0;
+  
 
   while( NULL != fgets( csv_line, sizeof(csv_line), fp_csv_file ) )
-  {
+  {  
     // Remove newline
     csv_line[(int32_t)strlen(csv_line)-1] = 0;
     
     // Parse csv line and populate array
-    parse_line( csv_line, p_value_table[line_index] ); 
-    
-    printf("\r\n");           
-    
-    if( line_index > MAX_SAMPLES )
-    {
-      // Don't want to overflow the array. Return error.
-      printf("OVERFLOW\n");
-      return 0;
-    }
+    parse_line( csv_line, p_value_table );         
     
     line_index++;
   }
