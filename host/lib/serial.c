48c0fa6231c2dccd1e587057dd5b8cb4c903bfd9
Alvaro Prieto <alvaro@alvaro-portege.(none)>
2011-09-06 12:25:42 -0400

    Removed trailing whitespace.

--- host/lib/serial.c
+++ host/lib/serial.c
@@ -28,16 +28,16 @@ uint8_t serial_open( int32_t port_number, int32_t baud_rate,
                                        void (*callback)( uint8_t*, uint32_t) )
 {
   serial_port_number = port_number;
-  // Use RS232 library to open serial port  
+  // Use RS232 library to open serial port
   if ( OpenComport( serial_port_number, baud_rate ) )
-  {    
+  {
     // Error opening serial port
     return 1;
   }
-  
+
   // Set a callback function for receiving packets
   serial_read_callback = callback;
-  
+
   return 0;
 }
 
@@ -52,30 +52,30 @@ void serial_close()
 
 /*******************************************************************************
  * @fn     void *serial_read_thread()
- * @brief  Run as thread. Polls serial port and builds packets. Calls 
+ * @brief  Run as thread. Polls serial port and builds packets. Calls
  *         serial_callback function with each new packet
  * ****************************************************************************/
 void *serial_read_thread()
 {
-  uint8_t serial_buffer[BUFFER_SIZE]; 
+  uint8_t serial_buffer[BUFFER_SIZE];
   uint8_t packet_buffer[BUFFER_SIZE];
   uint8_t final_buffer[BUFFER_SIZE];
-  
+
   uint16_t buffer_offset = 0;
   uint8_t new_packet = 0;
- 
+
   uint16_t bytes_read = 0;
-  
+
   printf("Starting serial read.\r\n");
-  
+
   // Flush the port
   while( PollComport( serial_port_number, serial_buffer, BUFFER_SIZE ) );
-  
+
   memset( serial_buffer, 0x00, sizeof(serial_buffer) );
-  
+
   // Run forever
   for(;;)
-  {  
+  {
     // Read a buffer full of data (if available)
     bytes_read = PollComport( serial_port_number, serial_buffer, BUFFER_SIZE );
     if( bytes_read > 0 )
@@ -87,23 +87,23 @@ void *serial_read_thread()
       }
       else
       {
-        
-        memcpy( packet_buffer + buffer_offset, serial_buffer, bytes_read );                        
-       
+
+        memcpy( packet_buffer + buffer_offset, serial_buffer, bytes_read );
+
         if ( packet_in_buffer( packet_buffer ) )
-        {          
-                             
+        {
+
           buffer_offset = 0;
-          
+
           new_packet = 1;
-          bytes_read = find_and_escape_packet( packet_buffer, final_buffer );          
-          
-        } 
+          bytes_read = find_and_escape_packet( packet_buffer, final_buffer );
+
+        }
         else
-        {   
-          
+        {
+
           buffer_offset += bytes_read;
-          
+
           if( buffer_offset > BUFFER_SIZE )
           {
             printf("Buffer has overflowed. Flushing.\n");
@@ -112,26 +112,26 @@ void *serial_read_thread()
             buffer_offset = 0;
           }
         }
-        
+
         if( new_packet == 1 )
         {
-          
+
           serial_read_callback( final_buffer, (bytes_read-1) );
-          
+
           new_packet = 0;
           memset( packet_buffer, 0x00, sizeof(packet_buffer) );
-          
-        }      
+
+        }
 
       }
     }
     memset( serial_buffer, 0x00, sizeof(serial_buffer) );
-    
-    // Don't take up all the processor time    
+
+    // Don't take up all the processor time
     usleep(2000);
 
   }
-  
+
 }
 
 /*******************************************************************************
@@ -149,22 +149,22 @@ uint8_t packet_in_buffer( uint8_t* old_buffer )
   {
     if( old_buffer[buffer_index] == SYNC_BYTE )
     {
-      sync_bytes++;      
+      sync_bytes++;
     }
     buffer_index++;
   }
-  
+
   // No packet found
   if ( sync_bytes < 2 )
   {
     return 0;
-  }  
-  
-  return 1;  
+  }
+
+  return 1;
 }
 
 /*******************************************************************************
- * @fn     uint16_t find_and_escape_packet( uint8_t* old_buffer, 
+ * @fn     uint16_t find_and_escape_packet( uint8_t* old_buffer,
  *                                                         uint8_t* new_buffer )
  * @brief  Find packet in buffer, remove escape characters, and return size
  * ****************************************************************************/
@@ -187,7 +187,7 @@ uint16_t find_and_escape_packet( uint8_t* old_buffer, uint8_t* new_buffer )
     buffer_index++;
   }
 
-  
+
   // process until next SYNC_BYTE is found
   while ( (start) & (buffer_index < BUFFER_SIZE) )
   {
@@ -209,12 +209,12 @@ uint16_t find_and_escape_packet( uint8_t* old_buffer, uint8_t* new_buffer )
   {
     return 0;
   }
-  return packet_size;  
+  return packet_size;
 }
 
 
 /*******************************************************************************
- * @fn     void send_serial_message( uint8_t* packet_buffer, 
+ * @fn     void send_serial_message( uint8_t* packet_buffer,
  *                                                        int16_t buffer_size )
  * @brief  Escape buffer and transmit through serial port
  * ****************************************************************************/
@@ -223,13 +223,13 @@ void send_serial_message( uint8_t* packet_buffer, int16_t buffer_size )
   uint8_t p_tmp_buffer[BUFFER_SIZE];
   uint8_t total_size = 0;
   uint8_t buffer_index = 0;
- 
+
   // Add start character
-  p_tmp_buffer[total_size++] = SYNC_BYTE; 
-   
+  p_tmp_buffer[total_size++] = SYNC_BYTE;
+
   for(; buffer_size > 0; buffer_size-- )
   {
-    if( ( SYNC_BYTE == packet_buffer[buffer_index] ) || 
+    if( ( SYNC_BYTE == packet_buffer[buffer_index] ) ||
         ( ESCAPE_BYTE == packet_buffer[buffer_index] ) )
     {
       p_tmp_buffer[total_size++] = ESCAPE_BYTE;
@@ -240,9 +240,9 @@ void send_serial_message( uint8_t* packet_buffer, int16_t buffer_size )
       p_tmp_buffer[total_size++] = packet_buffer[buffer_index++];
     }
   }
-  
+
   p_tmp_buffer[total_size++] = SYNC_BYTE;
-  
+
   SendBuf(serial_port_number, p_tmp_buffer, total_size);
 }
 
--- sim/readcsv/main.c
+++ sim/readcsv/main.c
@@ -28,87 +28,87 @@ double watt_to_dbm( double power );
 energy_t target_rssi;
 
 int32_t main ( int32_t argc, char *argv[] )
-{  
-  FILE *fp_in, *fp_out;      
+{
+  FILE *fp_in, *fp_out;
   energy_t rssi_table[MAX_DEVICES+1][MAX_DEVICES+1];
   uint32_t index = 0;
   char node_id_string[3];
   uint8_t node_index;
-  
+
   target_rssi = dbm_to_watt(-75l);
-  
+
   // Make sure the filename is included
   if ( argc < 3 )
   {
     printf( "Usage: %s infile outfile\r\n", argv[0] );
     return 1;
   }
-  
+
   // Open input csv file
   fp_in = fopen( argv[1], "r" );
-  
+
   if( NULL == fp_in )
   {
     printf( "Error opening input file.\r\n" );
     return 1;
   }
-  
+
   // Open output csv file
   fp_out = fopen( argv[2], "w" );
-  
+
   if( NULL == fp_out )
   {
     printf( "Error opening input file.\r\n" );
     return 1;
   }
-  
-  // Add nodes 
+
+  // Add nodes
   for( node_index = 0; node_index < (MAX_DEVICES + 1); node_index++ )
   {
     sprintf( node_id_string, "%d", node_index );
     add_labeled_node( node_index, 0, node_id_string );
   }
-  
+
   initialize_node_energy( 0 );
-  
+
   print_node_energy( 0, fp_out );
-  
-  //parse_table( fp_in, rssi_table ); 
-  while( parse_table( fp_in, rssi_table ) )  
-  {    
-    
-    clean_table( rssi_table );       
+
+  //parse_table( fp_in, rssi_table );
+  while( parse_table( fp_in, rssi_table ) )
+  {
+
+    clean_table( rssi_table );
 
     add_links_from_table( rssi_table );
-    
+
     // Run dijkstra's algorithm with 0 being the access point
     dijkstra( 0 );
-    
+
     // Display shortest paths and update energies
     for( node_index = 1; node_index < (MAX_DEVICES + 1); node_index++ )
     {
       compute_shortest_path( node_index );
       print_shortest_path( node_index );
     }
-    
+
     //print_all_links();
-    
+
     // Compute engergy mean and subtract from each individual mean
-    compute_mean_energy( 0 );    
-    
+    compute_mean_energy( 0 );
+
     printf("Round %d\n", index);
     print_node_energy( 0, fp_out );
-    
+
     //generate_graph( 0, index );
-    
-    
-    
+
+
+
     index++;
   }
 
-  fclose( fp_out );  
+  fclose( fp_out );
   fclose( fp_in );
-  
+
   return 0;
 }
 
@@ -121,20 +121,20 @@ void parse_line ( char* csv_line, energy_t* rssi_line )
 {
   char *p_item;
   uint16_t item_index = 0;
-  
+
   p_item = strtok( csv_line, "," );
   while( NULL != p_item )
-  {    
+  {
     // Convert string to RSSI value and store in rssi_line
     rssi_line[item_index] = rssi_values[(uint8_t)strtol( p_item, NULL, 10 )];
-                            
+
     item_index++;
     p_item = strtok( NULL, "," );
   }
 }
 
 /*******************************************************************************
- * @fn    uint8_t parse_table ( FILE* fp_csv_file, 
+ * @fn    uint8_t parse_table ( FILE* fp_csv_file,
  *                                      int8_t p_rssi_table[][MAX_DEVICES+1] )
  *
  * @brief Read lines from CSV file and parse them until an empty line is found
@@ -156,20 +156,20 @@ uint8_t parse_table ( FILE* fp_csv_file, energy_t p_rssi_table[][MAX_DEVICES+1] 
     {
       // Remove newline
       csv_line[(int32_t)strlen(csv_line)-1] = 0;
-      
+
       // Parse csv line and populate array
-      parse_line( csv_line, p_rssi_table[line_index] );            
+      parse_line( csv_line, p_rssi_table[line_index] );
     }
-    
+
     if( line_index > MAX_DEVICES )
     {
       // Don't want to overflow the array. Return error.
       return 0;
     }
-    
+
     line_index++;
   }
-  
+
   // Did not read table successfully
   return 0;
 }
@@ -182,7 +182,7 @@ uint8_t parse_table ( FILE* fp_csv_file, energy_t p_rssi_table[][MAX_DEVICES+1] 
 void clean_table( energy_t rssi_table[][MAX_DEVICES+1] )
 {
   uint16_t col_index, row_index;
-  
+
   for( row_index = 0; row_index < ( MAX_DEVICES+1 ); row_index++ )
   {
     for( col_index = row_index; col_index < ( MAX_DEVICES+1 ); col_index++ )
@@ -195,7 +195,7 @@ void clean_table( energy_t rssi_table[][MAX_DEVICES+1] )
     }
 
   }
-  
+
 }
 
 /*******************************************************************************
@@ -209,10 +209,10 @@ void add_links_from_table( energy_t rssi_table[][MAX_DEVICES+1] )
   energy_t link_power;
   energy_t tx_power;
   energy_t alpha;
-  
+
   // Constant tx power for now, will change later
   tx_power = dbm_to_watt(1.5l);
-  
+
   for( row_index = 0; row_index < ( MAX_DEVICES ); row_index++ )
   {
     for( col_index = row_index + 1; col_index < ( MAX_DEVICES+1 ); col_index++ )
@@ -220,10 +220,10 @@ void add_links_from_table( energy_t rssi_table[][MAX_DEVICES+1] )
       // Compute the minimum power required to meet this link with 'target_rssi'
       // alpha is the channel attenuation, that is received/transmitted power
       alpha = dbm_to_watt( rssi_table[row_index][col_index] ) / tx_power;
-      
+
       // Transmit power required is the target rssi / channel attenuation
       link_power = target_rssi / alpha;
-      
+
       // Add link
       add_link( row_index, col_index, link_power );
     }
@@ -249,4 +249,3 @@ double watt_to_dbm( double power )
 {
   return 10l * log10( 1000l * power );
 }
-
